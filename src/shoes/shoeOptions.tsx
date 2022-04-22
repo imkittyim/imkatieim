c27011c6adb1e1fec34a0ecd6d56e936aba62987
Katie Im <katieim@Katies-MBP.fios-router.home>
2022-04-22 00:09:50 -0400

    added two shoes

--- src/shoes/shoeOptions.tsx
+++ src/shoes/shoeOptions.tsx
@@ -2,10 +2,14 @@ import cortez1972 from './cortez1972';
 import blazer1973 from './blazer1973';
 import airforceone1982 from './airforceone1982';
 import michaeljordandunks1984 from './michaeljordandunks1984';
+import nikedunkhigh1985 from './nikedunkhigh1985';
+import airmaxone1987 from './airmaxone1987';
 
 export const shoeOptions = [
   cortez1972,
   blazer1973,
   airforceone1982,
-  michaeljordandunks1984
+  michaeljordandunks1984,
+  nikedunkhigh1985,
+  airmaxone1987
 ];
